779d1ab387d968bbeb649de5742fd732a8a89711
yoki <petras.moil@gmail.com>
2020-08-14 21:11:47 +0300

    sensor cards design

--- project/main.py
+++ project/main.py
@@ -1,13 +1,19 @@
 # main.py
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
+import datetime
 from database import db
+from models import Sensor
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
+    sensors = [sensor.as_dict() for sensor in Sensor.query.all()]
+    sensor = Sensor(name='Sensor 1', land_number="45",  last_update=datetime.datetime.now())
+    # db.session.add(sensor)
+    # db.session.commit()
     return render_template('index.html')
 
 
--- project/models.py
+++ project/models.py
@@ -12,7 +12,7 @@ class User(UserMixin, db.Model):
 class Carrier(db.Model):
     __tablename__ = 'carrier'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000), unique=True)
+    name = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(100))
     children = db.relationship("Contact")
 
@@ -23,11 +23,23 @@ class Carrier(db.Model):
 class Contact(db.Model):
     __tablename__ = 'contact'
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(1000))
+    name = db.Column(db.String(100))
     cell_number = db.Column(db.String(100))
     email = db.Column(db.String(100))
     carrier_id = db.Column(db.Integer, db.ForeignKey('carrier.id'))
     notify = db.Column(db.Boolean, default=True)
+
+    def as_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
+
+
+class Sensor(db.Model):
+    __tablename__ = 'sensor'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    land_number = db.Column(db.String(100), unique=True)
+    address = db.Column(db.String(100), unique=True)
+    last_update = db.Column(db.DateTime)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
