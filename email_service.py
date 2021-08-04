28bf31b615cc2b03769cad892566667887677446
yoki <petras.moil@gmail.com>
2021-08-04 15:59:38 +0300

    sms service

--- email_service.py
+++ email_service.py
@@ -1,7 +1,18 @@
 from flask import current_app
 import smtplib
+import telnyx
 from socket import gaierror
-from models import Contact, Carrier
+from models import Contact
+
+
+def send_sms(msg):
+    telnyx.api_key = current_app.config.get("TELNYX_KEY")
+    your_telnyx_number = current_app.config.get("TELNYX_NUMBER")
+
+    contacts = Contact.query.filter_by(notify=True).all()
+    for contact in contacts:
+        destination_number = f'+1{contact.cell_number}'
+        telnyx.Message.create(from_=your_telnyx_number, to=destination_number, text=msg, )
 
 
 def send_email(msg):
@@ -9,12 +20,13 @@ def send_email(msg):
     smtp_server = current_app.config.get("MAIL_SERVER")
     sender = username = current_app.config.get("MAIL_USERNAME")
     password = current_app.config.get("MAIL_PASSWORD")
-   
+
     contacts = Contact.query.filter_by(notify=True).all()
+    send_sms(msg)
     for contact in contacts:
         receiver = contact.email
         message = f"""Subject: DitchFlow Notification\n\n {msg}"""
-       
+
         try:
             with smtplib.SMTP(smtp_server, port) as server:
                 server.starttls()
@@ -30,6 +42,3 @@ def send_email(msg):
         except Exception as e:
             print("Another exception")
             print(str(e))
-            
-
-    
